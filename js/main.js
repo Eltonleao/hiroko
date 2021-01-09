c59817bc8461b6c70eab3e2bfe9d7b4d11690e8e
Eltonleao <3lton.leao@gmail.com>
2021-01-09 10:43:39 -0300

    E: separacao de components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var path = window.location.hostname;
+
 $(document).ready(function() {
     // $("#conteudo").load("dashboard.html");
 
@@ -9,14 +11,16 @@ function loadComponents() {
     var loaders = $('.load');
     var loader;
     var component;
+    var type;
     for (var i = 0; i < 2; i++) {
         loader = $(loaders[i]);
-        component = loader.data('component') + ".html";
+        type = loader.data('type');
+        if (type) {
+            component = "/components/" + type + "/" + loader.data('component') + ".html";
+        } else {
+            component = "/components/" + loader.data('component') + ".html";
+        }
         loader.load(component);
     }
 
-}
-
-function loadContent(loader, page) {
-    $("#" + loader).load(page + ".html");
 }
