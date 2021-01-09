6d1183aa51538165d5fc25292fdcbb69b6024ae7
Eltonleao <3lton.leao@gmail.com>
2021-01-09 11:35:08 -0300

    dashboard em andamento

--- js/main.js
+++ js/main.js
@@ -9,16 +9,22 @@ $(document).ready(function() {
 function loadComponents() {
 
     var loaders = $('.load');
+    console.log(loaders)
     var loader;
     var component;
     var type;
-    for (var i = 0; i < 2; i++) {
+    var id;
+    for (var i = 0; i < loaders.length; i++) {
         loader = $(loaders[i]);
+        id = loader.attr('id');
+        console.log(loader);
         type = loader.data('type');
         if (type) {
             component = "/components/" + type + "/" + loader.data('component') + ".html";
         } else {
+            // console.log('entrei aqui');
             component = "/components/" + loader.data('component') + ".html";
+            // console.log(component);
         }
         loader.load(component);
     }
